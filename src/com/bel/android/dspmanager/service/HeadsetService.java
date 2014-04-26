omnirom/android_packages_apps_DSPManager

[2/3] Effects: Add center frequency param to bassboost
Change-Id: Ic082790010ed9e9abaf0e3249034322234119c66

src/com/bel/android/dspmanager/service/HeadsetService.java
@@ -347,6 +347,7 @@ public class HeadsetService extends Service {
 
 		session.mBassBoost.setEnabled(preferences.getBoolean("dsp.bass.enable", false));
 		session.mBassBoost.setStrength(Short.valueOf(preferences.getString("dsp.bass.mode", "0")));
+		session.mBassBoost.setCenterFrequency(Short.valueOf(preferences.getString("dsp.bass.freq", "55")));
 
 		/* Equalizer state is in a single string preference with all values separated by ; */
 		session.mEqualizer.setEnabled(preferences.getBoolean("dsp.tone.enable", false));

SpiritBlood/packages_apps_DSPManager

Conflicts:
	res/values/arrays.xml
	res/xml/headset_preferences.xml
	src/com/bel/android/dspmanager/service/HeadsetService.java

src/com/bel/android/dspmanager/service/HeadsetService.java
@@ -284,6 +284,7 @@ public class HeadsetService extends Service {
         try {
             session.mBassBoost.setEnabled(prefs.getBoolean("dsp.bass.enable", false));
             session.mBassBoost.setStrength(Short.valueOf(prefs.getString("dsp.bass.mode", "0")));
+            session.mBassBoost.setCenterFrequency(Short.valueOf(prefs.getString("dsp.bass.freq", "55")));
         } catch (Exception e) {
             Log.e(TAG, "Error enabling bass boost!", e);
         }